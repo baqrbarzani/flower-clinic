a2099e23bd735eebd00880dd64d63aa9f7366929
baqrbarzani <baqr.00934879@gmail.com>
2025-04-16 21:15:36 +0300

    Create style.py

---
+++ style.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def set_custom_style():
+    st.markdown("""
+        <style>
+            .stButton>button {
+                border-radius: 10px;
+                background-color: #1f77b4;
+                color: white;
+            }
+        </style>
+    """, unsafe_allow_html=True)
