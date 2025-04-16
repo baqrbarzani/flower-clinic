281cf87732dba2bd30fd850e9d5514b618e5c31a
baqrbarzani <baqr.00934879@gmail.com>
2025-04-16 21:19:23 +0300

    Update style.py

--- style.py
+++ style.py
@@ -3,10 +3,11 @@ import streamlit as st
 def set_custom_style():
     st.markdown("""
         <style>
-            .stButton>button {
-                border-radius: 10px;
-                background-color: #1f77b4;
-                color: white;
-            }
+        .stButton>button {
+            border-radius: 10px;
+            background-color: #1f77b4;
+            color: white;
+            padding: 6px 12px;
+        }
         </style>
     """, unsafe_allow_html=True)
