7c2bf96279ed943c1559bd25ede81259cd01f5c8
baqrbarzani <baqr.00934879@gmail.com>
2025-04-18 11:07:06 +0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,21 +2,12 @@ import streamlit as st
 from database import get_db_connection
 
 def show_dashboard():
-    if "logged_in" not in st.session_state or not st.session_state.logged_in:
-        st.warning("Please log in to access the dashboard.")
-        return
-
-    st.title(f"Welcome to the Dashboard, Dr. {st.session_state.username}")
-
+    st.subheader("Dashboard")
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute("SELECT disease_name FROM diseases")
     diseases = cursor.fetchall()
     conn.close()
-
-    if diseases:
-        st.write("List of Diseases:")
-        for disease in diseases:
-            st.write(f"- {disease['disease_name']}")
-    else:
-        st.write("No diseases found.")
+    st.write("List of Diseases:")
+    for disease in diseases:
+        st.write(f"- {disease['disease_name']}")
