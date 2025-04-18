db5c28c5bd3a4c95ed57e05ed5ebf370962db455
baqrbarzani <baqr.00934879@gmail.com>
2025-04-18 11:15:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
 import streamlit as st
 from login import show_login
-from register import show_register
-from dashboard import show_dashboard
+from database import initialize_database
 
 def main():
-    st.title("Flower Clinic App")
-
-    if 'logged_in' not in st.session_state:
-        st.session_state['logged_in'] = False
-
-    if st.session_state['logged_in']:
-        show_dashboard()
-        if st.button("Logout"):
-            st.session_state['logged_in'] = False
-            st.session_state['username'] = ""
-    else:
-        menu = ["Login", "Register"]
-        choice = st.sidebar.selectbox("Menu", menu)
-        if choice == "Login":
-            show_login()
-        elif choice == "Register":
-            show_register()
+    initialize_database()
+    show_login()
 
 if __name__ == "__main__":
     main()
